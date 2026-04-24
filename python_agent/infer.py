19773268af6c29eaa1018599f87f7fe571ea4806
Aiswarya12-gpu <aiswaryaprajeeshlal@gmail.com>
2026-04-24 17:26:52 +0200

    Add RLInference class for reinforcement learning
    Implement RLInference class for model inference

--- python_agent/infer.py
+++ python_agent/infer.py
@@ -1,1 +1,21 @@
+import torch
+from model import PolicyNetwork
+from utils import normalize_state
 
+ACTION_SPACE = [-0.2, -0.1, 0.0, 0.1, 0.2]
+
+class RLInference:
+    def __init__(self, model_path="ppo_quic_model.pt"):
+        self.model = PolicyNetwork()
+        self.model.load_state_dict(torch.load(model_path))
+        self.model.eval()
+
+    def predict(self, rtt, loss, throughput):
+        state = normalize_state(rtt, loss, throughput)
+
+        state = torch.FloatTensor(state)
+        probs, _ = self.model(state)
+
+        action_idx = torch.argmax(probs).item()
+
+        return ACTION_SPACE[action_idx]
